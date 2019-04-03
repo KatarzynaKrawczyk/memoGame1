302fde1c9ca9c4d8d5e3f7e81cba210ec03e81b5
Katarzyna Krawczyk <krawczyk.kat@gmail.com>
2019-04-03 20:55:16 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,33 +1,23 @@
 import React, { Component } from 'react';
-
 import fallingStar from './fallingStar.png';
 import './App.css';
+import GameContainer from './container/GameContainer';
 
 class App extends Component {
-    componentDidMount() {
-
-    }
   render() {
     return (
-      
       <div className="App">
         <header className="App-header">
-          <h1>
-           MemoGame
-          </h1>
+          <h1>MemoGame</h1>
           <div class="container">
-            <div class="row p-2">
-              <div class="col-sm" className="card m-3">
-                <img src={fallingStar} className="flip" alt="fallingStar"/>
-              </div>
-              <div class="col-sm" className="card m-3">
-                <img src={fallingStar} className="flip" alt="fallingStar"/>
-              </div>
-              <div class="col-sm" className="card m-3">
-                <img src={fallingStar} className="flip" alt="fallingStar"/>
-              </div>
+            <div class="row">
+              <GameContainer/>
+              <GameContainer/>
+              <GameContainer/>
+              <GameContainer/>
             </div>
           </div> 
+          
         </header>
       </div>
     );
---
+++ src/container/GameContainer.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import fallingStar from '../fallingStar.png';
+
+class GameContainer extends React.Component {
+    render() {
+        return (
+            
+              <div class="col-sm" className="card m-3 mx-auto">
+                <img src={fallingStar} className="flip" alt="fallingStar"/>
+              </div>
+              
+        )
+            
+    }
+}
+
+export default GameContainer;
